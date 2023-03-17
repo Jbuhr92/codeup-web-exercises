6ceb0ee8c28e5f98910503908e739654798409ed
John Buhr <Johnbuhr92@gmail.com>
2023-03-17 11:35:59 -0500

    Finished jquery selector exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,4 +1,8 @@
 $(document).ready(function (){
    // alert("the page has loaded")
+$(`.codeup`).css('border', '1px solid red');
+$(`li`).css( 'font-size', '20px');
+$(`h1, p, li`).css('background-color', 'yellow');
+    alert($(`h1` ).contents().text())
 
 });
