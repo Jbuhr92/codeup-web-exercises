ceeb28cb60849a726aa8ddd59f35843294b19283
John Buhr <Johnbuhr92@gmail.com>
2023-03-30 11:10:53 -0500

    Finished promises exercises

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,21 @@
-import { fetchData } from './keys.js';
+import {keys} from './keys.js';
 
+const options = {
+    headers: {
+        'Authorization': `token ${keys.github}`
+    }
+};
+
+function fetchData(url) {
+    return fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch data');
+            }
+        });
+}
 function getLastCommitDate(username) {
     const url = `https://api.github.com/users/${username}/events/public`;
     return fetchData(url)
